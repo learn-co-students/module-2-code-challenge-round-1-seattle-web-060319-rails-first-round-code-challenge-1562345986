0a9465a0cd5947f467131fd6b315368b57bd34de
Karen Go <karenrgo@gmail.com>
2019-07-05 10:53:15 -0700

    part 1 complete

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -2,4 +2,31 @@ class HeroinesController < ApplicationController
   def index
     @heroines = Heroine.all
   end
+
+  def show
+    @heroine = Heroine.find(params[:id])
+  end
+
+  def new
+    @heroine = Heroine.new
+  end
+
+  def create
+    @heroine = Heroine.new(heroine_params)
+    if @heroine.save
+      redirect_to @heroine
+    else
+      flash.clear
+      flash[:super_name_error] = @heroine.errors.messages[:super_name].first
+      flash[:name_error] = @heroine.errors.messages[:name].first
+      render :new
+    end
+  end
+
+  private
+
+    def heroine_params
+      params.require(:heroine).permit(:name, :super_name, :power_id)
+    end
+
 end
