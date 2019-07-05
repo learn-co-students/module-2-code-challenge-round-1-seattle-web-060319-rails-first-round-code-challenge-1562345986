6a627f65938e20a6334e82dc05032cb2b2ac6368
Spencer Lindemuth <lindemut@gmail.com>
2019-07-05 10:30:12 -0700

    Finished Non Bonus Objectives

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -2,4 +2,27 @@ class HeroinesController < ApplicationController
   def index
     @heroines = Heroine.all
   end
+
+  def show
+    @heroine = Heroine.find(params[:id])
+  end
+
+  def new
+    @heroine = Heroine.new
+  end
+
+  def create
+    @heroine = Heroine.new(params.require(:heroine).permit(:name, :super_name, :power_id))
+    if @heroine.save
+      redirect_to @heroine
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
 end
