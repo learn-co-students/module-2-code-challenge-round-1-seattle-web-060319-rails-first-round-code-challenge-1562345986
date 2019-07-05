6aeda7d0cd898851a8f62c8d9b580f0c57145a99
Jason Leach <jlspursfan@gmail.com>
2019-07-05 10:55:09 -0700

    Basics completed

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -2,4 +2,31 @@ class HeroinesController < ApplicationController
   def index
     @heroines = Heroine.all
   end
+
+  def show
+    @heroine = Heroine.find(params[:id])
+  end
+
+  def new
+    @heroine = Heroine.new
+    @powers = Power.all
+  end
+
+  def create
+    @heroine = Heroine.new(heroine_params)
+
+    if @heroine.save
+      redirect_to @heroine
+    else
+      @powers = Power.all 
+      render :new
+    end
+  end
+
+  private
+
+  def heroine_params
+    params.require(:heroine).permit(:name, :super_name, :power_id)
+  end
+
 end
--- app/models/heroine.rb
+++ app/models/heroine.rb
@@ -1,2 +1,10 @@
 class Heroine < ApplicationRecord
+  validates :super_name, uniqueness: true
+    
+  belongs_to :power
+
+  def power_name
+    self.power.name
+  end
+
 end
